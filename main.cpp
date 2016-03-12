558aa0323d1ce0a9d04fe8337f72c76cf0ad3c2c
Pathf <vhenique@gmail.com>
2016-03-12 23:49:13 +0100

    Ajout de la fonction dedoublonne dans le main (pas fini et en phase de test)

--- main.cpp
+++ main.cpp
@@ -14,12 +14,50 @@
 
 #include <iostream>		// cout, endl
 #include "Test/test.hpp"	// Permet de verifier si les 
+#include "File/Anneau.hpp"
 
+template class Anneau<>;
+
+Anneau<> dedoublonne(Anneau<> A){
+	Anneau<> Asans;
+	int cpt = 0;
+
+//Permet de compter le nombre d'élément dans l'Anneau:
+	while(!A.estVide()){
+		Asans.ajoute(A.courant());
+		A.supprime();
+		cpt++;
+	}
+	
+	for(int i = 0; i < cpt; i++){
+		A.ajoute(Asans.courant());
+		Asans.avance();
+	}
+
+//Fait le tri :
+	T elt;
+	int cptB = 0;
+	for(int i = 0; ; i++){
+		elt = Asans.courant();
+		while(cpt - 1 > cptB){
+			Asans.avance();
+			if(elt == Asans.courant()){
+				Asans.supprime();
+				cpt--;
+			} else {
+				cptB++;
+			}
+		}
+		cptB = 0;
+	}
+	
+	return Asans;
+}
 
 int main (){
 	
 	// Permet de tester pour pile ou file en fonction de l'implementation
-	test();	
+	//test();	
 
 	return 0;
 }
