dd5da1d9e23dcff57fa2095faa4f752d7f2bf2fe
Pathf <vhenique@gmail.com>
2016-03-13 00:25:50 +0100

    Fonction dedoublonne et testDedoublonne en fonctionnement (il me reste à les rassembler dans le repertoire Test)

--- main.cpp
+++ main.cpp
@@ -16,10 +16,10 @@
 #include "Test/test.hpp"	// Permet de verifier si les 
 #include "File/Anneau.hpp"
 
-template class Anneau<>;
+template < typename T>
 
-Anneau<> dedoublonne(Anneau<> A){
-	Anneau<> Asans;
+Anneau<T> dedoublonne(Anneau<T> A){
+	Anneau<T> Asans;
 	int cpt = 0;
 
 //Permet de compter le nombre d'élément dans l'Anneau:
@@ -36,28 +36,65 @@ Anneau<> dedoublonne(Anneau<> A){
 
 //Fait le tri :
 	T elt;
-	int cptB = 0;
-	for(int i = 0; ; i++){
+	int cptB;
+
+	for(int i = 0; i < cpt; i++){
+		cptB = 0;
 		elt = Asans.courant();
+		Asans.avance();
 		while(cpt - 1 > cptB){
-			Asans.avance();
 			if(elt == Asans.courant()){
 				Asans.supprime();
 				cpt--;
 			} else {
+				Asans.avance();
 				cptB++;
 			}
 		}
-		cptB = 0;
+		Asans.avance();
+		Asans.avance();
+		cpt--;
 	}
-	
+
+	// Juste pour garder sons ordre de départ
+	Asans.avance();	
+
 	return Asans;
+}
+
+// fonction test de void dedoublonne(Anneau<> A);
+void testDedoublonne(){
+	Anneau<> A;
+	A.ajoute('a');
+	A.ajoute('b');
+	A.ajoute('c');
+	A.ajoute('a');
+	A.ajoute('c');
+	A.ajoute('d');
+	A.ajoute('a');
+
+	Anneau<> B = dedoublonne(A);
+
+	std::cout << "Anneau A (";
+	while(!A.estVide()){
+		std::cout << A.courant() << ",";
+		A.supprime();
+	}
+	std::cout << "FIN)" << std::endl;
+
+	std::cout << "Anneau B (";
+	while(!B.estVide()){
+		std::cout << B.courant() << ",";
+		B.supprime();
+	}
+	std::cout << "FIN)" << std::endl;
 }
 
 int main (){
 	
 	// Permet de tester pour pile ou file en fonction de l'implementation
 	//test();	
+	testDedoublonne();
 
 	return 0;
 }
